f2f3fc73e53bb9a6243d998d87bc3bf9c5086612
Carlos Lopez <carloslopezr29@gmail.com>
2023-10-27 11:05:31 -0400

    TodoCount component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './platzi.webp';
 import './App.css';
 
+// component
 function App() {
   return (
     <div className="App">
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +19,32 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
+}
+
+function TodoItem(){
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+
+function TodoCount(){
+  return(
+    <h1>
+      You completed
+      3 of
+      5 TO DO'S
+    </h1>
+  )
 }
 
 export default App;
