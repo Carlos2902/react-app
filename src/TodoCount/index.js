7ab6ae4c3ba3bbf8f5edacb8cab908d4b6a059b4
Carlos Lopez <carloslopezr29@gmail.com>
2023-12-15 12:33:08 -0500

    React.Context hook added into TodoCount

--- src/TodoCount/index.js
+++ src/TodoCount/index.js
@@ -1,19 +1,24 @@
-// const estilos = {
-//   backgroundColor: 'red'
-// }
-
+import React from 'react';
 import './TodoCount.css';
+import { TodoContext } from '../TodoContext';
 
 
-function TodoCount({total, completed}){
+function TodoCount(){
+  // we not longer receive props in the parameter, now we receive it 
+  // IN the global context
+  // For instance, we need to send what context is the one we wanna use into the useContext hook
+  const {
+    completedTodos,
+    totalTodos,
+  }=React.useContext(TodoContext)
  
   return(
-    completed === total?
+    completedTodos === totalTodos?
     <h1 className='allFinished'>
       Congratulations, you've acomplished everything. STAY HARD!!💪🏽
     </h1>:
      <h1 className='TodoCount'>
-      You've acomplished <span>{completed}</span> of <span>{total}</span>  TO DO's
+      You've acomplished <span>{completedTodos}</span> of <span>{totalTodos}</span>  TO DO's
      </h1>
     );
   }
